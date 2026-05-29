044064becf99dd96badec35c4128e11ca8ebed7f
unknown <yorlanprato95@gmail.com>
2026-05-29 17:36:28 -0400

    Remove base path to fix Vercel MIME type error

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,6 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/sargentopimienta-2/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
